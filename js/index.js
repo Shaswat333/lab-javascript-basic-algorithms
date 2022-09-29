66ac30d45a928fbf5ef46ffdb9d5c778b2247fda
Shaswat333 <shaswatpachhera21@gmail.com>
2022-09-29 20:55:29 +0100

    lab done

--- js/index.js
+++ js/index.js
@@ -1,6 +1,24 @@
 // Iteration 1: Names and Input
 //
+let hacker1 = "nissan";
+    console.log(`the driveers name is ${hacker1}`);
+let hacker2 = "TATA";
+    console.log(`the navigators name is ${hacker2}`)
 // Iteration 2: Conditionals
-
+for (let i=0; i<1; i++){
+    if (hacker1.length >hacker2.length) {
+        console.log(`${hacker1} has longer name`);
+    } else if  (hacker1.length<hacker2.length ) {
+        console.log(`${hacker2} has longer name`);
+    } else {
+        console.log(`${hacker1} and ${hacker2} has equal length`)
+    }
+}
 
 // Iteration 3: Loops
+let hackers = "";
+for (let i=0; i< hacker1.length;i++){
+    hackers += hacker1[i].toUpperCase() + " ";    
+}
+console.log(hackers);
+    
